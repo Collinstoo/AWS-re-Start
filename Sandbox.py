8e41f1ba2c6997a59730786fefd96170a84cd5b0
Collins Too <collotoo254@gmail.com>
2025-10-15 15:41:38 +0300

    Wednesday

--- Sandbox.py
+++ Sandbox.py
@@ -1,5 +1,4 @@
-import math
-absolute=5.999
-floor_test=198.42
-
-result=math.fabs(absolute)
+pi=3.14159
+r=input("Enter the radius of the circle: ")
+area=pi*r**2
+print("The area of the circle is: ",area)
