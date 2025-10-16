6c9dd0031bbb5c4913915f849153e3cc03220716
Collins Too <collotoo254@gmail.com>
2025-10-16 10:29:07 +0300

    Thursday

--- Sandbox.py
+++ Sandbox.py
@@ -1,4 +1,10 @@
-pi=3.14159
-r=input("Enter the radius of the circle: ")
-area=pi*r**2
-print("The area of the circle is: ",area)
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero.")
+    return a / b
